b11012e97095def68782b5db8f348a23b4103a77
iamfarhatsharefi <sharefifarhat@gmail.com>
2024-08-20 03:50:25 -0700

    trying for working on index.js file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import App from './App';
+// eslint-disable-next-line import/extensions
+import reportWebVitals from './reportWebVitals';
+/* eslint-enable no-unused-vars */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Report web vitals metrics
 reportWebVitals();
