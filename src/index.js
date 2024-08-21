889177757cce19c3efe6b41ab2b057008d794f6b
iamfarhatsharefi <sharefifarhat@gmail.com>
2024-08-21 08:26:52 -0700

    eslient code editing

--- src/index.js
+++ src/index.js
@@ -1,39 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Updated import for React 18
 import './index.css';
 
 const RandomQuoteMachine = () => {
-    const [quote, setQuote] = useState('');
-    const [author, setAuthor] = useState('');
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
 
-    const fetchQuote = async () => {
-        const response = await fetch('https://api.quotable.io/random');
-        const data = await response.json();
-        setQuote(data.content);
-        setAuthor(data.author);
-    };
+  const fetchQuote = async () => {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    setQuote(data.content);
+    setAuthor(data.author);
+  };
 
-    useEffect(() => {
-        fetchQuote();
-    }, []);
+  useEffect(() => {
+    fetchQuote();
+  }, []);
 
-    return (
-        <div id="quote-box">
-            <p id="text">{quote}</p>
-            <p id="author">- {author}</p>
-            <button id="new-quote" onClick={fetchQuote}>
-                New Quote
-            </button>
-            <a
-                id="tweet-quote"
-                href={`https://twitter.com/intent/tweet?text=${quote} - ${author}`}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Tweet Quote
-            </a>
-        </div>
-    );
+  return (
+    <div id="quote-box">
+      <p id="text">{quote}</p>
+      <p id="author">- {author}</p>
+      <button id="new-quote" onClick={fetchQuote} type="button">
+        New Quote
+      </button>
+      <a
+        id="tweet-quote"
+        href={`https://twitter.com/intent/tweet?text=${quote} - ${author}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Tweet Quote
+      </a>
+    </div>
+  );
 };
 
-ReactDOM.render(<RandomQuoteMachine />, document.getElementById('quote-box'));
+// Updated to use createRoot
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RandomQuoteMachine />);
